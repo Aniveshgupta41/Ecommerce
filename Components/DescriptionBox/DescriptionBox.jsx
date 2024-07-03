ab3d19afa0b1923f9152e67f4f3828da7691c223
Aniveshgupta41 <169152867+Aniveshgupta41@users.noreply.github.com>
2024-07-03 10:53:23 +0530

    Add files via upload

---
+++ Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>An e-commerce website is one that allows people to buy and sell physical goods, services, and digital products over the internet rather than at a brick-and-mortar location. Through an e-commerce website, a business can process orders, accept payments, manage shipping and logistics, and provide customer service.eCommerce website is an online portal that facilitates online transactions of goods and services through means of the transfer of information and funds over the Internet. In the early days, e-commerce was done partially through emails and phone calls.</p>
+        <p>You can sell just about anything through an eCommerce site. Examples include recreational equipment, car and motorcycle parts, household goods, clothing and jewelry, and food and drinks. If you need help coming up with additional ideas, consider these wholesale items to sell or high demand products.</p>
+      </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
